c433fdd5682c3b4253efabca09b66809215ef9fd
yaohui.fang <yaohui.fang@space.top>
2024-07-04 15:05:24 +0800

    Maven环境配置

---
+++ GRPCDemo/GrpcDemoServer/src/main/java/com/example/grpcdemoserver/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.grpcdemoserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @FileName DemoController
+ * @Description
+ * @Author yaoHui
+ * @date 2024-07-04
+ **/
+@RestController
+
+public class DemoController {
+}
