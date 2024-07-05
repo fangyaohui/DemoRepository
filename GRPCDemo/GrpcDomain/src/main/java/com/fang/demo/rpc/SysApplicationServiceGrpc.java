4ecfcd0fcf55f5e9b1ee9e7d8b6fa815f1b86db9
yaohui.fang <yaohui.fang@space.top>
2024-07-05 10:40:02 +0800

    grpc自动生成文件

--- GRPCDemo/GrpcDomain/src/main/java/com/fang/demo/rpc/SysApplicationServiceGrpc.java
+++ GRPCDemo/GrpcDomain/src/main/java/com/fang/demo/rpc/SysApplicationServiceGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.42.2)",
+    value = "by gRPC proto compiler (version 1.42.1)",
     comments = "Source: Application.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class SysApplicationServiceGrpc {
