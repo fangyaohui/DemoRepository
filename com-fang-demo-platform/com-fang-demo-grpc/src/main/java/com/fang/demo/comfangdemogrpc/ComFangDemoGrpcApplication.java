d77eef7d47bc84694f96934baebc19d7ea209290
yaohui.fang <yaohui.fang@space.top>
2024-07-15 11:04:07 +0800

    Nacos启动成功

---
+++ com-fang-demo-platform/com-fang-demo-grpc/src/main/java/com/fang/demo/comfangdemogrpc/ComFangDemoGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.fang.demo.comfangdemogrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComFangDemoGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComFangDemoGrpcApplication.class, args);
+    }
+
+}
--- com-fang-demo-platform/nacosDemo/src/main/java/com/example/nacosdemo/NacosDemoApplication.java
+++ com-fang-demo-platform/nacosDemo/src/main/java/com/example/nacosdemo/NacosDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.comfangdemonacosnomaven;
+package com.example.nacosdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ComFangDemoNacosNoMavenApplication {
+public class NacosDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ComFangDemoNacosNoMavenApplication.class, args);
+        SpringApplication.run(NacosDemoApplication.class, args);
     }
 
 }
