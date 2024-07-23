35022dc56a621f64bb185721849e42b3bddecf81
yaohui.fang <yaohui.fang@space.top>
2024-07-23 17:06:26 +0800

    Dubbo 依赖包

--- com-fang-demo-base/com-fang-demo-upm/src/main/java/com/fang/demo/comfangdemoupms/ComFangDemoUpmApplication.java
+++ com-fang-demo-base/com-fang-demo-upm/src/main/java/com/fang/demo/comfangdemoupms/ComFangDemoUpmApplication.java
@@ -1,11 +1,12 @@
 package com.fang.demo.comfangdemoupms;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
+@EnableDubbo
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(value = {"com.fang.demo.comfangdemocommunal.*","com.fang.demo.comfangdemoupms.*"})
--- com-fang-demo-base/com-fang-demo-upm/src/main/java/com/fang/demo/comfangdemoupms/service/impl/UserServiceImpl.java
+++ com-fang-demo-base/com-fang-demo-upm/src/main/java/com/fang/demo/comfangdemoupms/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.fang.demo.comfangdemoupms.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fang.demo.comfangdemoupms.mapper.UserInfoMapper;
 import com.fang.demo.comfangdemoupms.service.UserService;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Service;
 import po.UserInfoPO;
 
@@ -14,6 +15,7 @@ import java.util.List;
  * @Author yaoHui
  * @date 2024-07-17
  **/
+@DubboService
 @Service
 public class UserServiceImpl extends ServiceImpl<UserInfoMapper, UserInfoPO> implements UserService {
 
